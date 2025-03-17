cada3757d2e89b51bd536494d36f77f57a32ce28
Davi Oliveira <davi.ol.santos@gmail.com>
2025-03-17 09:01:47 -0300

    criando header

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import { History } from './pages/History'
 import { Home } from './pages/Home'
-import { DefaultLayout } from './layouts/DefaultLayout'
+import { DefaultLayout } from './layouts/DefaultLayout/index'
 
 export function Router() {
   return (
